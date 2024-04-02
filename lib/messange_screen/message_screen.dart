b036df0802dc98ebfa8ed02c1d6c5f48951628ca
beomsuong <beomsuong0406@gmail.com>
2024-04-02 22:08:14 +0900

    웹소캣 체팅방 추가

--- lib/messange_screen/message_screen.dart
+++ lib/messange_screen/message_screen.dart
@@ -1,12 +1,83 @@
-///채팅방 목록 화면
-import 'package:flutter/widgets.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-class MessageScreen extends ConsumerWidget {
-  const MessageScreen({super.key});
+class MessageScreen extends StatefulWidget {
+  const MessageScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+  _MessageScreenState createState() => _MessageScreenState();
+}
+
+class _MessageScreenState extends State<MessageScreen> {
+  late IO.Socket socket;
+  final TextEditingController _controller = TextEditingController();
+  List<String> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Socket.IO 클라이언트 초기화 및 연결
+    socket = IO.io('http://10.0.2.2:3000', <String, dynamic>{
+      'transports': ['websocket'],
+    });
+    socket.onConnect((_) {
+      socket.emit('join room', 'room1');
+    });
+    socket.on('chat message', (data) {
+      print("메시지 수신 $data");
+      setState(() {
+        messages.add(data); // 메시지 리스트에 메시지 추가
+      });
+    });
+    socket.onDisconnect((_) {
+      print('disconnect');
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    socket.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("채팅방 예시"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                itemCount: messages.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(messages[index]),
+                  );
+                },
+              ),
+            ),
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(labelText: 'Send a message'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_controller.text.isNotEmpty) {
+                  socket.emit('chat message',
+                      {'msg': _controller.text, 'room': 'room1'});
+                  _controller.clear();
+                }
+              },
+              child: const Text('Send Message'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
